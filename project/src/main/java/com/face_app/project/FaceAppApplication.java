39ee3118405607c03981a8f6978b281d48869f1c
Jefferson Pedro <jeffersonpedro05@gmail.com>
2025-12-02 15:06:29 -0300

    adicionando as dependencias do JavaCV

---
+++ project/src/main/java/com/face_app/project/FaceAppApplication.java
@@ -0,0 +1,13 @@
+package com.face_app.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaceAppApplication.class, args);
+	}
+
+}
