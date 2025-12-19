77fc3296ecc2d3ca5aaaba66acdecd5afbedb0fb
Jefferson Pedro <jeffersonpedro05@gmail.com>
2025-12-19 20:27:16 -0300

    Create Usuario.java

---
+++ project/src/main/java/com/face_app/project/model/Usuario.java
@@ -0,0 +1,122 @@
+package com.face_app.project.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+
+@Entity
+@Table
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idusuario")
+    private Integer idUsuario;
+
+    @NotNull
+    @Length(min = 3, max = 150)
+    @Column(name = "nome")
+    private String nome;
+
+    @NotNull
+    @Length(min = 11, max = 11)
+    private String cpf;
+
+    @NotNull
+    @Length(min = 11, max = 100)
+    @Email
+    private String email;
+
+    @NotNull
+    @Length(min = 10, max = 20)
+    private String telefone;
+
+    @NotNull
+    @Length(min = 10, max = 45)
+    private String login;
+
+    @NotNull
+    @Length(min = 6, max = 255)
+    private String senha;
+
+    @NotNull
+    private Boolean ativo;
+
+    public Usuario(Integer idUsuario, String nome, String cpf, String email, String telefone, String login, String senha, Boolean ativo) {
+        this.idUsuario = idUsuario;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.telefone = telefone;
+        this.login = login;
+        this.senha = senha;
+        this.ativo = ativo;
+    }
+
+    public Usuario() {}
+
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
+}
