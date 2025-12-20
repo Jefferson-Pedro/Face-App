a41f718106d13f2e27fffc49c9e1d76350edbda6
Jefferson Pedro <jeffersonpedro05@gmail.com>
2025-12-19 21:13:17 -0300

    Create UsuarioController.java

---
+++ project/src/main/java/com/face_app/project/controller/UsuarioController.java
@@ -0,0 +1,33 @@
+package com.face_app.project.controller;
+
+import com.face_app.project.dto.UsuarioDTO;
+import com.face_app.project.model.Usuario;
+import com.face_app.project.service.IUsuarioService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
+public class UsuarioController {
+
+    @Autowired
+    private IUsuarioService service;
+
+    @PostMapping("/user/new")
+    public ResponseEntity<String> register (@Valid @RequestBody UsuarioDTO usuarioDTO){
+        try{
+            Boolean res = service.register(usuarioDTO);
+            if(res){
+                return ResponseEntity.status(201).body("Usuario " + usuarioDTO.nome() + " cadastrado com sucesso!");
+            }
+            return  ResponseEntity.status(409).body("Usuário já cadastrado na base de dados!");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("ERRO: Dados incompletos! " + e);
+        }
+    }
+}
